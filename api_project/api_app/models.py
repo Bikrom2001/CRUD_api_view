29cb5cf3a80b81c1ab86a43039fe4a0611afff74
Bikrom Roy <bikramroy2001@gmail.com>
2026-08-15 14:53:45 +0600

    models and admin register add done

--- api_project/api_app/models.py
+++ api_project/api_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class StudentModel(models.Model):
+    
+    name = models.CharField(max_length=100, null=True)
+    email = models.EmailField(null=True)
+    address = models.TextField(null=True)
+    deparment = models.CharField(max_length=100, null=True)
+    
+    def __str__(self):
+        return f'{self.name}'
+
+
