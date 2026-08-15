c9151dfcb101e46ab6bc0a7aa44ef76bc495a533
Bikrom Roy <bikramroy2001@gmail.com>
2026-08-15 15:26:09 +0600

    serializers done

---
+++ api_project/api_app/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from api_app.models import *
+
+
+class StudentSerializers(serializers.ModelSerializer):
+    
+    class Meta:
+        model: StudentModel
+        fields = '__all__'
+        
+    
+
