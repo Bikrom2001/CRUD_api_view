35168ee39ba9e0c3976decf53794f577d30e830e
Bikrom Roy <bikramroy2001@gmail.com>
2026-08-15 15:44:04 +0600

    GET method done for user

--- api_project/api_app/views.py
+++ api_project/api_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from api_app.serializers import *
+from api_app.models import *
 
-# Create your views here.
+
+@api_view(['GET'])
+
+def student_list(request):
+    
+    if request.method == 'GET':
+        student_data = StudentModel.objects.all()
+        serializer_data = StudentSerializers(student_data, many=True) 
+        return Response(serializer_data.data)
+
