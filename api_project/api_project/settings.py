bcd1abac0a2b2bee7ac8411d016c4e43bf995bdd
Bikrom Roy <bikramroy2001@gmail.com>
2026-08-15 14:43:32 +0600

    project steup done

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a%5@3rx!i4c_^81(tx@grf_(tgu9@!%)glsaa*8r1lt1^j&*ol'
+SECRET_KEY = 'django-insecure-)gcv9!4w+*!j&)z-b_6!lq8=lm^fpp1ljb&e*_-3xjekwv*($a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
