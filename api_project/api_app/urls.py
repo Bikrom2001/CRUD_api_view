e90e35db3e865b6c84c626633cbf0476ddd2e9a3
Bikrom Roy <bikramroy2001@gmail.com>
2026-08-15 16:54:21 +0600

    add_student for post method

--- api_project/api_app/urls.py
+++ api_project/api_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from api_app.views import *
 
 urlpatterns = [
-    path('student-list/', student_list, name='student_list')
+    path('student-list/', student_list, name='student_list'),
+    path('add-student/', add_student, name='add_student'),
 ]
 
--- api_project/api_app/views.py
+++ api_project/api_app/views.py
@@ -17,3 +17,22 @@ def student_list(request):
             'data': serializer_data.data
         })
 
+
+
+@api_view(['POST'])
+
+def add_student(request):
+    
+    if request.method == 'POST':
+        serializer_data = StudentSerializers(data = request.data)
+        if serializer_data.is_valid():
+            serializer_data.save()
+            return Response({
+                "success": True,
+                "message": "student created Successfully",
+                'data': serializer_data.data  
+            })
+        return Response({
+            'success': False,
+            'error': serializer_data.errors
+        })
